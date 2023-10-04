9f44d67ca335242415149be56fa867b69083ce35
Fernando Gomez <dim.dei@gmail.com>
2023-10-04 16:56:56 -0500

    Actualizac_04/10-16:56-

--- Frontend_2/js/script.js
+++ Frontend_2/js/script.js
@@ -28,22 +28,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var previousButton = document.getElementById('previous');
     var nextButton = document.getElementById('next');
 
+    var numeroPagina = parseInt(window.location.pathname.match(/\d+/)[0]); // Extrae el número de la página actual desde la URL
+
     previousButton.addEventListener('click', function(event) {
         event.preventDefault();
-        window.location.href = 'proyecto012.html';
+        var paginaAnterior = (numeroPagina - 1) <= 0 ? 12 : numeroPagina - 1; // Si es la página 1, la anterior es la 12
+        window.location.href = 'proyecto' + paginaAnterior.toString().padStart(3, '0') + '.html'; // Genera la URL de la página anterior
     });
 
     nextButton.addEventListener('click', function(event) {
         event.preventDefault();
-        window.location.href = 'proyecto002.html';
+        var paginaSiguiente = (numeroPagina + 1) > 12 ? 1 : numeroPagina + 1; // Si es la página 12, la siguiente es la 1
+        window.location.href = 'proyecto' + paginaSiguiente.toString().padStart(3, '0') + '.html'; // Genera la URL de la página siguiente
     });
-});
-/*=======OBTENER PAGINA ANTERIOR======== */
-document.addEventListener('DOMContentLoaded', function() {
-    var paginaAnterior = document.referrer;
-    if (paginaAnterior) {
-        console.log('Página anterior:', paginaAnterior);
-    } else {
-        console.log('No hay página anterior');
-    }
 });
