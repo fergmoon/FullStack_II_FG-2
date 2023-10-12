55bc904a84e8d08851afceae9bab2d10b3ff57db
Fernando Gomez <dim.dei@gmail.com>
2023-10-11 20:29:23 -0500

    actualiz_10-11: 20:29-

--- Frontend_2/js/script.js
+++ Frontend_2/js/script.js
@@ -9,23 +9,24 @@ class headermain extends HTMLElement{
         <nav>
             <ul>
                 <li>                
-                    <a href="home.html">home</a>
+                    <a href="index.html">home</a>
                 </li>            
                 <li id="dropdown" class="dropdown">
                     <a class="dropbtn" href="work.html">Work</a>
                     <div class="dropdown-content">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                        <a href="#">Link 4</a>
-                        <a href="#">Link 5</a>
-                        <a href="#">Link 6</a>
-                        <a href="#">Link 7</a>
-                        <a href="#">Link 8</a>
-                        <a href="#">Link 9</a>
-                        <a href="#">Link 10</a>
-                        <a href="#">Link 11</a>
-                        <a href="#">Link 12</a>
+                        <a href="proyecto001.html">Vitrina Punto de Venta</a>
+                        <a href="proyecto002.html">Iluminación Navideña</a>
+                        <a href="proyecto003.html">Mobiliario Hogar 01</a>
+                        <a href="proyecto004.html">Estantes Cocina</a>
+                        <a href="proyecto005.html">Punto de Venta Itinerante 01</a>
+                        <a href="proyecto006.html">Proyectos especiales 01</a>
+                        <a href="proyecto007.html">Escritorio Pequeño</a>
+                        <a href="proyecto008.html">Punto de venta Itinerante 02</a>
+                        <a href="proyecto009.html">Exhibición Comercial</a>
+                        <a href="proyecto010.html">Exhibidor Móvil</a>
+                        <a href="proyecto011.html">Mobiliario Hogar 02</a>
+                        <a href="proyecto012.html">Proyectos Especiales 02</a>
+
                     </div>                    
                 </li>
                 <li>
@@ -59,3 +60,5 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'proyecto' + paginaSiguiente.toString().padStart(3, '0') + '.html'; // Genera la URL de la página siguiente
     });
 });
+
+
